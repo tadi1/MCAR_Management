c52435c31f765ed46cf39fc7dc465cef0638fe76
Takari <takaridonaville@gmail.com>
2016-12-18 06:33:46 -0600

    Added MedicationsController

---
+++ MusicCityAnimalRescueManagement/Controllers/MedicationsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MusicCityAnimalRescueManagement.Models;
+using MusicCityAnimalRescueManagement.Models.Animals;
+
+namespace MusicCityAnimalRescueManagement.Controllers
+{
+    public class MedicationsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Medications
+        public ActionResult Index()
+        {
+            return View(db.Medications.ToList());
+        }
+
+        // GET: Medications/Details/5
+        public ActionResult Details(short? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Medication medication = db.Medications.Find(id);
+            if (medication == null)
+            {
+                return HttpNotFound();
+            }
+            return View(medication);
+        }
+
+        // GET: Medications/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Medications/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,Name,isFleaTick,isHeartworm,isForCats,isForDogs,isDewormer")] Medication medication)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Medications.Add(medication);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(medication);
+        }
+
+        // GET: Medications/Edit/5
+        public ActionResult Edit(short? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Medication medication = db.Medications.Find(id);
+            if (medication == null)
+            {
+                return HttpNotFound();
+            }
+            return View(medication);
+        }
+
+        // POST: Medications/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,Name,isFleaTick,isHeartworm,isForCats,isForDogs,isDewormer")] Medication medication)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(medication).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(medication);
+        }
+
+        // GET: Medications/Delete/5
+        public ActionResult Delete(short? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Medication medication = db.Medications.Find(id);
+            if (medication == null)
+            {
+                return HttpNotFound();
+            }
+            return View(medication);
+        }
+
+        // POST: Medications/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(short id)
+        {
+            Medication medication = db.Medications.Find(id);
+            db.Medications.Remove(medication);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
