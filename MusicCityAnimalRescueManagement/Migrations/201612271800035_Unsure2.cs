38d1020e0ed81f17713da8929cab6e0a6b9a1d9e
Takari <takaridonaville@gmail.com>
2016-12-27 12:31:31 -0600

    Tweaks to CatVetting

---
+++ MusicCityAnimalRescueManagement/Migrations/201612271800035_Unsure2.cs
@@ -0,0 +1,18 @@
+namespace MusicCityAnimalRescueManagement.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Unsure2 : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Animals", "InRescueTime", c => c.Double());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Animals", "InRescueTime", c => c.String());
+        }
+    }
+}
